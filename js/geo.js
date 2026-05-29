a3b29e4ddc33849303d04f25a77a08eac6ed975e
SamuDevGit <samu.davargas@gmail.com>
2026-05-28 21:17:37 -0500

    fix geo

--- js/geo.js
+++ js/geo.js
@@ -34,8 +34,8 @@ function mostrarMapa(lat, lng) {
         className: '',
         html: `
         <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16 30s10-12.27 10-18A10 10 0 1 0 6 12c0 5.73 10 18 10 18z" fill="#e53935" stroke="#b71c1c" stroke-width="2"/>
-          <circle cx="16" cy="13" r="4" fill="#fff" stroke="#b71c1c" stroke-width="2"/>
+          <path d="M16 30s10-12.27 10-18A10 10 0 1 0 6 12c0 5.73 10 18 10 18z" fill="#06cf0c" stroke="#06cf0c" stroke-width="2"/>
+          <circle cx="16" cy="13" r="4" fill="#fff" stroke="#06cf0c" stroke-width="2"/>
         </svg>
         `,
         iconSize: [32, 32],
@@ -44,6 +44,6 @@ function mostrarMapa(lat, lng) {
     });
 
     L.marker([lat, lng], { icon: customIcon }).addTo(window._mapa)
-        .bindPopup('<b>¡Soy un perrito!</b><br><img src="img/perrito.jpg" alt="Ubicación" width="100">')
+        .bindPopup('<b>¡Aqui estoy!</b><br><img src="img/logo.png" alt="Ubicación" width="100">')
         .openPopup();
 }
